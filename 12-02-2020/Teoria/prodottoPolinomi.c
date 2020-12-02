442a71cb317e377f5087e666573b0c3a8e8192b7
fedesegala <segala.federico@gmail.com>
2020-12-02 11:30:47 +0100

    compilato file prodotto polinomi

--- 12-02-2020/Teoria/prodottoPolinomi.c
+++ 12-02-2020/Teoria/prodottoPolinomi.c
@@ -10,7 +10,7 @@ void main(){
     int g1, g2, g3; //grado del polinomio
     int p1[MAX+1];  //lunghezza = grado, il +1 è per il termine noto 
     int p2[MAX+1];
-    int p3[MAX*2+1] = 0;   //prodotto, il grado sarà MAX + MAX
+    int p3[MAX*2+1];;   //prodotto, il grado sarà MAX + MAX
 
     //acquisisco i due gradi e calcolo il terzo
     scanf("%d",&g1);
